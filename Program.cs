c38ff04f1eb171664c1c9325fa59acebcf62d96a
muradpunisher <muradsm@code.edu.az>
2023-11-20 09:55:57 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Məsələ bir: [input olaraq ilin aylarının hər hansı biri daxil edilir, mən consola hansı fəsil olduğunu yazdırmalıyam]
+        Console.WriteLine("Enter a month name:");
+        string month = Console.ReadLine().ToLower();
+
+        if (month == "december" || month == "january" || month == "february")
+        {
+            Console.WriteLine("Winter");
+        }
+        else if (month == "march" || month == "april" || month == "may")
+        {
+            Console.WriteLine("Spring");
+        }
+        else if (month == "june" || month == "july" || month == "august")
+        {
+            Console.WriteLine("Summer");
+        }
+        else if (month == "september" || month == "october" || month == "november")
+        {
+            Console.WriteLine("Autumn");
+        }
+        else
+        {
+            Console.WriteLine("You have entered the wrong month name.");
+        }
+
+    }
+}
+
