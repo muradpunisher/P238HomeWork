76394afa7cc5e48e6bdb82da49122b83958379da
muradpunisher <muradsm@code.edu.az>
2023-11-20 10:18:00 +0400

    commitment

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main()
     {
-        // Məsələ bir: [input olaraq ilin aylarının hər hansı biri daxil edilir, mən consola hansı fəsil olduğunu yazdırmalıyam]
+        // Məsələ bir: [ İnput olaraq ilin aylarının hər hansı biri daxil edilir, mən consola hansı fəsil olduğunu yazdırmalıyam. Yanlış ay adı daxil edilərsə ekrana yazdırmalıyam ki, yanlış ad daxil etdiniz.]
         Console.WriteLine("Enter a month name:");
         string month = Console.ReadLine().ToLower();
 
@@ -30,6 +30,29 @@ class Program
             Console.WriteLine("You have entered the wrong month name.");
         }
 
+        // Məsələ iki: (İnput olaraq integr bir dəyər gəlir mən bu ədədin neçə mərtəbəli olduğunu consola yazdırmalıyam.)
+        Console.WriteLine("Enter an integer value to determine the number of floors:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int floorCount = 1;
+        while (number >= 10)
+        {
+            number /= 10;
+            floorCount++;
+        }
+        Console.WriteLine($"The number of floors is: {floorCount}");
+
+        // Məsələ üç: {Daxil edilən ədədin faktorialını tap. (3!=3*2*1)}
+        Console.WriteLine("Enter a number to find its factorial:");
+        int factorialNumber = Convert.ToInt32(Console.ReadLine());
+
+        int factorial = 1;
+        for (int i = 1; i <= factorialNumber; i++)
+        {
+            factorial *= i;
+        }
+        Console.WriteLine($"The factorial of {factorialNumber} is: {factorial}");
+
+        Console.ReadKey();
     }
 }
-
